941fca0d822e9ae3f09a92647270fda4bbce16f7
DucJS <mrtranduc1994@outlook.com>
2020-09-15 09:35:22 +0700

    add nav top header

--- src/features/HeaderComponent/index.jsx
+++ src/features/HeaderComponent/index.jsx
@@ -1,18 +1,48 @@
 import React from "react";
-import {} from "antd";
-import { Col, Container, Row } from "reactstrap";
+import { Col, Row } from "antd";
+import {
+  BellOutlined,
+  FacebookOutlined,
+  InstagramOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
 
 export default function HeaderComponent() {
   return (
     <div className="header-component">
-      <Container fluid>
-        <Row>
-          <Col>
-            <div className="header-component__top">Header top here</div>
-            <div className="header-component__bottom">header bottom here</div>
-          </Col>
-        </Row>
-      </Container>
+      <div className="header-component__top">
+        <div className="header-component__nav-top-left">
+          <div>
+            <span>Kênh Người Bán</span>
+          </div>
+          <div>
+            <span>Tải ứng dụng</span>
+          </div>
+          <div>
+            <span>Kết nối </span>
+            <FacebookOutlined />
+            <InstagramOutlined />
+          </div>
+        </div>
+        <div className="header-component__nav-top-center"></div>
+        <div className="header-component__nav-top-right">
+          <div>
+            <BellOutlined />
+            <span>Thông Báo</span>
+          </div>
+          <div>
+            <QuestionCircleOutlined />
+            <span>Trợ Giúp</span>
+          </div>
+          <div>
+            <span>Đăng Ký</span>
+          </div>
+          <div>
+            <span>Đăng Nhập</span>
+          </div>
+        </div>
+      </div>
+      <div className="header-component__bottom">header bottom here</div>
     </div>
   );
 }
