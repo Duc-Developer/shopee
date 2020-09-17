b9bf8956ddc09edeaeb97132feeb02725fce6cf0
DucJS <mrtranduc1994@outlook.com>
2020-09-17 10:00:39 +0700

    fake title catalog item

--- src/features/Catalog/index.jsx
+++ src/features/Catalog/index.jsx
@@ -14,40 +14,80 @@ const catalogLogos = importAll(
   )
 );
 
+const titles = [
+  "Mẹ và Bé",
+  "Quà tặng",
+  "Điện Gia dụng",
+  "Điện thoại và phụ kiện",
+  "Nhà sách online",
+  "Ô tô, xe máy, xe đạp",
+  "Thời trang trẻ em",
+  "Giày dép nữ",
+  "Thời trang nam",
+  "Thể thao & Du lịch",
+  "Giày dép nam",
+  "Thời trang nữ",
+  "Thiết bị điện gia dụng",
+  "Đồng hồ",
+  "Phụ kiện thời trang",
+  "Thiết bị điện tử",
+  "Voucher",
+  "Mỹ phẩm & Làm đẹp",
+  "Máy tính & Laptop",
+  "Bách hóa online",
+  "Chăm sóc nhà cửa",
+  "Đồ ăn thú cưng",
+  "Đồ chơi",
+  "Máy ảnh",
+  "Túi ví",
+];
+
+function convertData() {
+  let result = [];
+  for (var key in titles) {
+    result.push({
+      src: catalogLogos[key],
+      title: titles[key],
+    });
+  }
+  return result;
+}
+
 export default function Catalog(props) {
   const refEl = useRef(null);
+
+  const catalogList = convertData();
   return (
     <div className="catalog-main">
       <div className="catalog-main__title">
         <h3>DANH MỤC</h3>
       </div>
-      <div 
-      ref={refEl} 
-      className="catalog-main__catalog-list"
-      >
+      <div ref={refEl} className="catalog-main__catalog-list">
         <div className="catalog-main__catalog-list--top">
-          {catalogLogos.slice(0, 13).map((item, index) => {
+          {catalogList.slice(0, 13).map((item, index) => {
             return (
               <div key={index}>
                 <CatalogCard
                   width="10.3em"
                   height="13em"
-                  src={item}
-                  title="something"
+                  src={item.src}
+                  alt={item.src}
+                  title={item.title}
                 />
               </div>
             );
           })}
         </div>
         <div className="catalog-main__catalog-list--bottom">
-          {catalogLogos.slice(13).map((item, index) => {
+          {catalogList.slice(13).map((item, index) => {
             return (
               <div key={index}>
                 <CatalogCard
                   width="10.3em"
                   height="13em"
-                  src={item}
-                  title="something"
+                  src={item.src}
+                  alt={item.src}
+                  title={item.title}
                 />
               </div>
             );
