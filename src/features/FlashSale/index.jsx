38d7dd44355c60234e2bd9ab2531e92e5231ac70
DucJS <mrtranduc1994@outlook.com>
2020-09-19 10:07:56 +0700

    create flash sale container default

--- src/features/FlashSale/index.jsx
+++ src/features/FlashSale/index.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ProductCard from '../../components/ProductCard';
+import React, { useRef, useState } from "react";
+import PropTypes from "prop-types";
+import ProductCard from "../../components/ProductCard";
+import TimeCountDown from "../../components/TimeCountDown";
+import data from "../../fakeData.json";
+import { Carousel } from "antd";
+import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
 
 export default function FlashSaleContainer(props) {
-    return(
-        <div className="flash-sale-container">
-            <ProductCard flashSale={true} />
+  const start = Math.floor(Math.random() * (data.products.length - 16));
+  const productSlices = data.products.slice(start, start + 15);
+  const innerRef = useRef(null);
+
+  return (
+    <div className="flash-sale-container">
+      <div className="flash-sale-container__header">
+        <div className="flash-sale-container__title">
+          <span>FLASH SALE</span>
         </div>
-    )
+        <TimeCountDown width="8em" hours={2} minutes={0} seconds={20} />
+      </div>
+      <div>
+        <Carousel ref={innerRef} autoplay={false} dots={false}>
+          {[0, 4, 9].map((num, index) => {
+            return (
+              <div key={index}>
+                {productSlices.slice(num, num + 5).map((item) => {
+                  const { src, name, price, sold, sale, maxFlash } = item;
+                  return (
+                    <div
+                      key={item.id}
+                      className="flash-sale-container__product"
+                    >
+                      <ProductCard
+                        src={src}
+                        name={name}
+                        price={price}
+                        sold={sold}
+                        sale={sale}
+                        maxFlash={maxFlash}
+                        width="16.3em"
+                        flashSale={true}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
+      <div className="flash-sale-container__button-group">
+        <LeftCircleOutlined
+          onClick={() => {
+            innerRef.current.prev();
+          }}
+          className="flash-sale-container__button-back-icon"
+        />
+        <RightCircleOutlined
+          onClick={() => {
+            innerRef.current.next();
+          }}
+          className="flash-sale-container__button-next-icon"
+        />
+      </div>
+    </div>
+  );
 }
